798ab2f1192e45ccec06fe65dc668a2a8a1e8427
haris2021 <gharis2k20@gmail.com>
2023-08-17 16:14:56 -0400

    Invite therapist finished in search component

--- Components/ScreenComponents/AskScreen.js
+++ Components/ScreenComponents/AskScreen.js
@@ -1,6 +1,8 @@
-import React,{useEffect, useState} from 'react';
-import { View, Text, TextInput, StyleSheet, ActivityIndicator, FlatList, Image, TouchableOpacity} from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { View, Text, TextInput, StyleSheet, ActivityIndicator, FlatList, Image, TouchableOpacity, Share} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+
 
 import filter from "lodash.filter";
 
@@ -17,19 +19,18 @@ const AskScreen = () => {
   const[Data, setData] = useState([]);
   const[error,setError] = useState(null);
   const[fullData, setfullData] = useState("");
-  const[search, setsearch] = useState(false);
 
   useEffect( ()=>{
 
     setisLoading(true);
-    fetchData(API_endpoint)
+    fetchData(API_endpoint);
 
   },[])
 
   const handleSearch = (query) =>
   {
       setSearchQuery(query);
-
+      console.log(query+ " ");
       const formattedquery = query.toLowerCase();
 
       const filtereddata = filter(fullData, (user) => {
@@ -38,8 +39,15 @@ const AskScreen = () => {
 
       });
 
+      filtereddata.forEach((user, index) => {
+        console.log(`User ${index + 1}:`);
+        console.log('Name:', user.name);
+        console.log('Email:', user.email);
+        // Add more properties you want to log
+      });
+      
       setData(filtereddata);
-      setsearch(!search);
+      console.log("Setting data inside handle search " + Data.length);
 
   }
 
@@ -101,15 +109,35 @@ const AskScreen = () => {
 
   }
 
+  const handleIconClick = async () => {
+    const result = await Share.share({
+      message:
+        'React Native | A framework for building native apps using React',
+    });
+    if (result.action === Share.sharedAction) {
+      if (result.activityType) {
+        // shared with activity type of result.activityType
+      } else {
+        // shared
+      }
+    } else if (result.action === Share.dismissedAction) {
+      // dismissed
+    }
+  };
+
+
+	// Navigation.addListener("focus", focusOnInput);
+
   return (
 
-    <SafeAreaView  style={{flex:1, marginHorizontal:80, top:20}} >  
+    <SafeAreaView  style={{flex:1, marginHorizontal:40, top:20}} >  
 
         <TextInput
                         placeholder="Find a therapist.."
                         clearButtonMode = "always"
                         autoCapitalize = "none"
-                        autoCorrect={false}                        
+                        autoCorrect={false}    
+                        autoFocus={true}                     
                         style={styles.searchbox}   
                         value = {SearchQuery}    
                         onChangeText = { (query) => handleSearch(query)}     
@@ -123,42 +151,64 @@ const AskScreen = () => {
                   
                   renderItem = {  ({item}) => (
 
-                      <View>
-                      
-                        <View style={styles.itemContainer}>
+                      <View style={{display:'flex', flexDirection: 'row',flexWrap: 'wrap', top:20, paddingBottom:20}}>
 
-                                      <Image 
+                           <View>
+
+                                       <Image 
                                                 source ={{uri :item.picture.thumbnail}}
                                                 style={styles.image}
                                       />   
 
-                                      <Text style={styles.textName}>{item.name.first}</Text> 
 
-                                      <Ionicons
-                                              name="md-checkmark-circle-outline" // The name of the icon you want to use
-                                              size={17}     // Size of the icon
-                                              color="black" // Color of the icon
-                                              style={{ marginLeft:10}}
-                                        />
-                          </View>
+                           </View> 
+                           
+                            <View>
 
-                          <View style={{flexDirection:'row',alignItems:'center'}}>
+                                  <View style={{left:20,display:'flex', flexDirection: 'row' }}>
 
-                                      <Text style={styles.textEmail}>{item.email}</Text>
+                                          <Text style={styles.textName}>{item.name.first}</Text> 
 
-                                      <View>
-                                                    <TouchableOpacity style={styles.button}>
+                                            <Ionicons
+                                                    name="md-checkmark-circle-outline" // The name of the icon you want to use
+                                                    size={17}     // Size of the icon
+                                                    color="black" // Color of the icon
+                                                    style={{ marginLeft:10}}
+                                              />
+
+                                  </View>
+
+                                  <Text style={styles.textEmail}>{item.email}</Text>
+
+                            </View>
+
+                            <View style={{ flexDirection: 'row', alignItems: 'center', paddingTop: 5, marginLeft:12}}>
+
+                                                   <TouchableOpacity style={styles.button}>
                                                       <Text style={styles.buttonText}>Ask</Text>
                                                     </TouchableOpacity>
 
-                                      </View>
 
-                          </View>
-
+                            </View>
                       </View>
 
                   ) }
             />  
+        }
+
+        {/* Display this view when search result is empty */}
+        { 
+          <View style={{ display: 'flex', alignItems: 'center', top:"10%"}}>
+
+            <Text style={{color:'grey', fontWeight:'bold'}}>Don't see your therapist</Text>
+
+            <View>
+                                                    <TouchableOpacity style={styles.invitebutton} onPress={handleIconClick}>
+                                                      <Text style={styles.buttonText} >Invite your therapist</Text>
+                                                    </TouchableOpacity>
+            </View>
+
+          </View>
         }
 
     </SafeAreaView>
@@ -168,30 +218,31 @@ const AskScreen = () => {
 
 const styles = StyleSheet.create({
   searchbox : 
-  {
+  { 
                                     borderColor: '#ccc',
                                     borderWidth: 1,
                                     borderRadius: 18, 
                                     backgroundColor: 'white',
                                     paddingHorizontal: 30, // Add some horizontal padding for better appearance
-                                    paddingvertical : 10,
+                                    paddingVertical: 10,
                                     height:40,
-                                    width:300
+                                    width:380
   },
   textName:{
-    fontSize:17,
+    fontSize:18,
     marginLeft:10,
     fontWeight:"600"
   },
   textEmail :{
-    fontSize:14,
-    marginLeft:10,
+    fontSize:15,
+    marginLeft:30,
     color:"grey"
   },
   image:{
     width:35,
     height:30,
-    borderRadius:65
+    left:10,
+    borderRadius:70
   },
   itemContainer :{
     flexDirection:'row',
@@ -200,17 +251,26 @@ const styles = StyleSheet.create({
     marginRight:10,
     marginTop:20
   },
-  
+  invitebutton:
+  {
+    backgroundColor: 'lightgreen',
+    paddingVertical: 25,
+    paddingHorizontal: 22,
+    borderRadius: 13,
+    left:10,
+    top:30
+  },
   button: {
     backgroundColor: 'lightgreen',
     paddingVertical: 5,
-    paddingHorizontal: 20,
+    paddingHorizontal: 18,
     borderRadius: 13,
     left:10
   },
   buttonText: {
     color: 'white',
     fontSize: 18,
+    fontWeight:'bold'
   },
 });
 
